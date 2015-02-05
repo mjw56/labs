801256c9fce0eec84622f44cb7550c606a427229
Mike Wilcox <mwilcox56@gmail.com>
2015-02-04 21:16:32 -0500

    update

--- react-classes-lab/src/components/HelloMessage.ts
+++ react-classes-lab/src/components/HelloMessage.ts
@@ -4,11 +4,14 @@ import React = require('react');
 
 class HelloMessage extends React.Component {
 
+  componentDidMount() {
+      console.log('make call to get some data');
+  }
+
   render() {
     return React.createElement('div', { className: this.props.name }, 'Hello ' + this.props.name);
   }
 
 };
-
 
 React.render(React.createElement(HelloMessage, { name: 'Mike' }), document.body);
