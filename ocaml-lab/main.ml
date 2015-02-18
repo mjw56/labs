c9a9785eef81b6ea50d05cb7051c65aca3b6b401
Mike Wilcox <mwilcox56@gmail.com>
2015-02-17 22:16:25 -0500

    beginner ocaml stuff

--- ocaml-lab/main.ml
+++ ocaml-lab/main.ml
@@ -1,4 +1,35 @@
+(* Hello World! *)
+
 let hello_world = fun ~name:n ->
   print_string ("Hello, " ^ n ^ "!!");;
 
-hello_world ~name:"World"
+(* Average *)
+let average a b =
+  (a +. b) /. 2.0;;
+
+let avg = average 178.9876 15.2;;
+(* print_float (avg);; *)
+
+(* algebra *)
+let num = ( * ) ((+) 3 4) ((/) 35 (( * ) 2 5));;
+print_int (num); print_newline();;
+
+(* recursion *)
+let rec f n = if n == 1 then 1 else n * f (n-1);;
+(*print_int (f 6);;*)
+
+(* for loop *)
+for i=1 to 10 do  print_int (( * ) i 3); print_string ".. " done; print_newline() ;;
+
+(* user input *)
+let rec hilo n =
+   print_string "type a number: ";
+   let i = read_int () in
+   if i = n then print_string "bingo!\n\n"
+    else
+      begin
+        if i < n then print_string "go higher\n" else print_string "go lower\n"  ;
+        hilo n
+      end ;;
+
+hilo 73;;
